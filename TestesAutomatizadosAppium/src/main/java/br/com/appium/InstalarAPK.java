6740b888bc3714a978ba2fcd5c61c4a035bc2ba9
joao.victorio <joaovitorvictorio@gmail.com>
2023-07-19 19:29:29 -0300

    Foi incluído o arquivo APK do aplicativo na pasta src/main/resources e criada uma nova classe para instalar o APK através do JUnit.

---
+++ TestesAutomatizadosAppium/src/main/java/br/com/appium/InstalarAPK.java
@@ -0,0 +1,39 @@
+package br.com.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class InstalarAPK {
+
+	@Test
+	public void deveSomarDoisValores() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "emulador");
+		desiredCapabilities.setCapability("automationName", "uiautomator2");
+		desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
+		desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),
+				desiredCapabilities);
+
+		MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("2");
+		el1.click();
+		MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("plus");
+		el2.click();
+		MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("2");
+		el3.click();
+		MobileElement el4 = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_preview");
+		
+		Assert.assertEquals("4", el4.getText());
+		
+		driver.quit();
+	}
+}
