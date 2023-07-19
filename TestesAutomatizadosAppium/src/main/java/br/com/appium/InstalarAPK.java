955f2d0a3d9bf7d2c70de26153c9da349b1726a5
joao.victorio <joaovitorvictorio@gmail.com>
2023-07-19 19:33:03 -0300

    Alterado o nome das classes que estavam invertidas

--- TestesAutomatizadosAppium/src/main/java/br/com/appium/InstalarAPK.java
+++ TestesAutomatizadosAppium/src/main/java/br/com/appium/InstalarAPK.java
@@ -3,37 +3,27 @@ package br.com.appium;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class InstalarAPK {
 
 	@Test
-	public void deveSomarDoisValores() throws MalformedURLException {
+	public void deveInstalarAPK() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setCapability("platformName", "Android");
 		desiredCapabilities.setCapability("deviceName", "emulador");
 		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability("appPackage", "com.google.android.calculator");
-		desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+		desiredCapabilities.setCapability(MobileCapabilityType.APP,
+				"C:\\Users\\JN\\Documents\\MEUS PROJETOS\\Projetos\\Pasta-de-estudo\\Testes-automatizados\\TestesAutomatizadosMobileAppium\\TestesAutomatizadosAppium\\src\\main\\resources\\CTAppium_1_2.apk");
 
 		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),
 				desiredCapabilities);
 
-		MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("2");
-		el1.click();
-		MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("plus");
-		el2.click();
-		MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("2");
-		el3.click();
-		MobileElement el4 = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_preview");
-		
-		Assert.assertEquals("4", el4.getText());
-		
 		driver.quit();
 	}
 }
